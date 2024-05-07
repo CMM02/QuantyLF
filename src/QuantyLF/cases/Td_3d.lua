8b08641f0b766dd9b7ea594f10b3e57689370dbe
Carl Magnus Meier <phr542@usask.ca>
2024-05-07 13:25:11 -0600

    fix relative import issue for lua files

--- src/QuantyLF/cases/Td_3d.lua
+++ src/QuantyLF/cases/Td_3d.lua
@@ -1,5 +1,13 @@
 -- import slater integral values
-require "src.QuantyLF.cases.utils.slater_integrals"
+function addRelPath(dir)
+    local spath = debug.getinfo(1, 'S').source:sub(2):gsub("^([^/])", "./%1"):gsub("[^/]*$", "")
+    dir = dir and (dir .. "/") or ""
+    spath = spath .. dir
+    package.path = spath .. "?.lua;" .. spath .. "?/init.lua" .. package.path
+end
+
+addRelPath('utils')
+require "slater_integrals"
 
 TimeStart("LF_RIXS")
 -- this example calculates the resonant inelastic x-ray scattering in Transition Metal 3d we look at the

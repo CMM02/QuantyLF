751af345b0609fcefeac73daa3c2e0afca860ff0
Carl Magnus Meier <phr542@usask.ca>
2024-05-27 10:54:18 -0600

    testing with respect to previous code done

--- local-QuantyLF.py
+++ local-QuantyLF.py
@@ -1,63 +1,64 @@
+# import package
 from QuantyLF.QuantyLF import QuantyLF
 
-
+# create new instance
 quantyLF = QuantyLF()
+# set custom quanty command
 quantyLF.set_quanty_command('/Users/phr542/Documents/Quanty/Quanty_macOS', 'Darwin')
 
+# load experimental data including excitation energies
 quantyLF.load_exp_xas('XAS_Exp.dat')
-quantyLF.load_exp_rixs('RIXS_Exp.dat', [638, 639.35, 640.16, 640.75])
+quantyLF.load_exp_rixs('RIXS_Exp.dat', [637.85, 639.2, 640.01, 640.6])
 
+# configure edge jump
+# set display to True to see a plot of experimental data along edge jump (display has to be set to false for calculation)
 quantyLF.config_edge_jump([[637.7, 0.14, 4], [648.2, 0.006, 8]], [600, 700,], display=False)
 
-# print(qunatyLF.available_cases())
-quantyLF.load_case('D3h_3d', manual=False)
-# qunatyLF.load_custom_case('./src/QuantyLF/cases/Td_3d.lua')
+# see available cases
+# print(quantyLF.available_cases())
+# load case and print parameters for fitting
+quantyLF.load_case('Oh_3d', manual=False)
 
 # Set up ion and oxidation state
-quantyLF.add_par('ion', 22, from_file=False)
-quantyLF.add_par('oxy', 4, from_file=False)
-quantyLF.add_par('Gamma1', 0.4120250470353196, [0.4, 1])
+quantyLF.add_par('ion', 25, from_file=False)
+quantyLF.add_par('oxy', 2, from_file=False)
+quantyLF.add_par('Gamma1', 0.9555234791474667, [0.4, 1])
 
-# # Crystal field contribution in D4h symmetry
-quantyLF.add_par('tenDq', 0.19, [-0.2, 0.2])
-quantyLF.add_par('tenDqF', 0.6815489483432551, [0.01, 1.0])
+# Crystal field contribution in D4h symmetry
+quantyLF.add_par('tenDq', 1.0021399464823173, [0, 2])
+quantyLF.add_par('tenDqF', 0.3547998065298788, [0.01, 1.0])
 
+# Multiplet contribution
+# Spin orbit coupling
+quantyLF.add_par('zeta_2p', 0.9955160828232357, [0.8, 1.02])
+quantyLF.add_par('zeta_3d', 0.8454338988348356, [0.8, 1.02])
+quantyLF.add_par('Xzeta_3d', 0.9241414520496289, [0.8, 1.02])
 
-quantyLF.add_par('Ds', 0.999, [0.1, 1])
-quantyLF.add_par('Dt', 0.99, [0.1, 1])
-quantyLF.add_par('DsF', 0.999, [0.1, 1])
-quantyLF.add_par('DtF', 0.999, [0.1, 1])
-
-# # Multiplet contribution
-# # spin orbit coupling
-quantyLF.add_par('zeta_2p', 1.0196625781428472, [0.8, 1.02])
-quantyLF.add_par('zeta_3d', 0.8403012992370478, [0.8, 1.02])
-quantyLF.add_par('Xzeta_3d', 1.0, [0.8, 1.02])
-
-# # Slater integrals (Coulomb repulsion/exchange correlation)
-quantyLF.add_par('Fdd', 0.9397729329705585, [0.8, 1.0])
-quantyLF.add_par('XFdd', 0.8137253445941214, [0.8, 1.0])
-quantyLF.add_par('Fpd', 0.8098173584848158, [0.8, 1.0])
-quantyLF.add_par('Gpd', 0.8053014352519605, [0.8, 1.0])
+# Slater integrals (Coulomb repulsion/exchange correlation)
+quantyLF.add_par('Fdd', 0.8009986743790085, [0.8, 1.0])
+quantyLF.add_par('XFdd', 0.9998855686772277, [0.8, 1.0])
+quantyLF.add_par('Fpd', 0.8030824105722778, [0.8, 1.0])
+quantyLF.add_par('Gpd', 0.8000030853156131, [0.8, 1.0])
 
 
-# # Ligand field contribution
-# # on-site energies (usually drops out of the equation in crystal field theory)
-quantyLF.add_par('Udd', 6.543685631427877, [2.0, 7.0])
-quantyLF.add_par('Upd_Udd', 4.001467895225598, [0.5, 5.0])
+# Ligand field contribution
+# on-site energies (usually drops out of the equation in crystal field theory)
+quantyLF.add_par('Udd', 2.8041196328436917, [2.0, 7.0])
+quantyLF.add_par('Upd_Udd', 1.954221327028076, [0.1, 2.0])
 
-# # Crystal field contribution of ligand site
-quantyLF.add_par('tenDqL', 0.022975132006965073, [0.01, 1.0])
+# Crystal field contribution of ligand site
+quantyLF.add_par('tenDqL', 0.9975196109261688, [0.01, 1.0])
 
-# # Charge transfer contribution
-quantyLF.add_par('Delta', 4.040660314729548, [1.0, 5.0])
+# Charge transfer contribution
+quantyLF.add_par('Delta', 3, [1.0, 5.0], from_file=False)
 
-# # Hybridization
-quantyLF.add_par('VfScale', 0.9775495515653867, [0.8, 1.0])
+# Hybridization
+quantyLF.add_par('VfScale', 0.8461434962013847, [0.8, 1.0])
 
 
 # # XAS and RIXS broadening
 quantyLF.add_broadening('XAS', [[-3.7, 0.5], [3, 0.7], [9, 0.7]], gamma=0.5)
-quantyLF.add_broadening('RIXS', [[0, 0.3], [2.7, 0.5], [8, 1]])
+quantyLF.add_broadening('RIXS', [[0, 0.3], [3, 0.8], [4,0.9], [6,1]])
 
+# Run calculation in 'RIXS' mode
 quantyLF.fit('RIXS')
